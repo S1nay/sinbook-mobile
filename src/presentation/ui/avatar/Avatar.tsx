232ee6ddc849b0c0b7331ab88aba3dfa3bb2135a
Sharkov Nikita <bigbasspoll@gmail.com>
2025-09-13 12:22:14 +0300

    config: added children for avatar

--- src/presentation/ui/avatar/Avatar.tsx
+++ src/presentation/ui/avatar/Avatar.tsx
@@ -1,15 +1,17 @@
-import { memo } from 'react';
+import { memo, PropsWithChildren } from 'react';
 import { Pressable } from 'react-native';
 import TurboImage from 'react-native-turbo-image';
 
 import { AvatarProps } from './types';
 
-const Avatar = (props: AvatarProps) => {
-  const { uri, size = 28, imageProps, ...otherProps } = props;
+const Avatar = (props: PropsWithChildren<AvatarProps>) => {
+  const { uri, size = 28, imageProps, children, ...otherProps } = props;
 
   return (
     <Pressable {...otherProps}>
       <TurboImage {...imageProps} source={{ uri }} style={{ width: size, height: size }} />
+
+      {children}
     </Pressable>
   );
 };
