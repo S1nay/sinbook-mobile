ce063f7e1b4127a384593ad9b08736f0b140336c
Sharkov Nikita <bigbasspoll@gmail.com>
2025-09-14 22:41:10 +0300

    chore: remove useSignedOut hook

--- src/core/providers/AuthProvider/index.ts
+++ src/core/providers/AuthProvider/index.ts
@@ -1,6 +1,5 @@
 import AuthContext from './AuthContext';
 import AuthProvider from './AuthProvider';
 import useSignedIn from './useSignedIn';
-import useSignedOut from './useSignedOut';
 
-export { AuthContext, AuthProvider, useSignedIn, useSignedOut };
+export { AuthContext, AuthProvider, useSignedIn };
--- src/core/providers/AuthProvider/useSignedOut.ts
+++
@@ -1,7 +0,0 @@
-import useSignedIn from './useSignedIn';
-
-const useSignedOut = () => {
-  return !useSignedIn();
-};
-
-export default useSignedOut;
