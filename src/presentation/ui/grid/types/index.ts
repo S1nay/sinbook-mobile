bb2ff6d3acc38f985f81758061e0b210a66faf00
Sharkov Nikita <bigbasspoll@gmail.com>
2025-11-03 12:22:18 +0300

    feat: created Grid ui component

---
+++ src/presentation/ui/grid/types/index.ts
@@ -0,0 +1,15 @@
+import { ReactElement } from 'react';
+import { FlatListProps } from 'react-native';
+
+export interface GridProps<T> extends Omit<FlatListProps<T>, 'renderItem'> {
+  numberOfColumns: number;
+  gap: number;
+  renderItem: GridRenderItem<T>;
+}
+
+export type GridRenderItem<T> = (info: GridRenderItemInfo<T>) => ReactElement | null;
+
+export type GridRenderItemInfo<T> = {
+  item: T;
+  style: { width: number; height: number };
+};
