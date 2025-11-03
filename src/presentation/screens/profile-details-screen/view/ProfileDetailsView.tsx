3fc8b7311e956e8b82b63207cd7beefdc73fdb9f
Sharkov Nikita <bigbasspoll@gmail.com>
2025-11-03 12:47:39 +0300

    feat: created ProfileDetailsView

--- src/presentation/screens/profile-details-screen/view/ProfileDetailsView.tsx
+++ src/presentation/screens/profile-details-screen/view/ProfileDetailsView.tsx
@@ -1,19 +1,29 @@
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useRoute } from '@react-navigation/native';
 import { observer } from 'mobx-react-lite';
 import { useEffect } from 'react';
 import { View } from 'react-native';
 
 import Header from '@components/header';
 import { useAuth, useDIContainer } from '@core/hooks';
+import AppLayout from '@layouts/_app';
 import { ProfileRouteNames, ProfileScreenProps } from '@navigation/configuration';
+import Button from '@ui/button';
 
 import { IProfileDetailsViewModel } from '../view-model';
+import styles from './styles';
+import ProfileInfo from '../components/profile-info';
+import ProfileInfoPlaceholder from '../components/profile-info-placeholder';
+import ProfilePostGrid from '../components/profile-post-grid';
+import ProfilePostGridPlaceholder from '../components/profile-post-grid-placeholder';
 
 const ProfileDetailsView = () => {
   const container = useDIContainer();
   const navigation =
     useNavigation<ProfileScreenProps<ProfileRouteNames.ProfileDetails>['navigation']>();
-  const { logout } = container.get(IProfileDetailsViewModel.$);
+  const { params } = useRoute<ProfileScreenProps<ProfileRouteNames.ProfileDetails>['route']>();
+  const { logout, user, getUserData, getUserPosts, posts, postsMeta } = container.get(
+    IProfileDetailsViewModel.$,
+  );
   const { unauthorize } = useAuth();
 
   const handleLogout = () => {
@@ -21,12 +31,42 @@ const ProfileDetailsView = () => {
   };
 
   useEffect(() => {
-    navigation.setOptions({
-      header: props => <Header {...props} rightIcon="logout" onPressRightIcon={handleLogout} />,
-    });
+    getUserData(params?.userId);
   }, []);
 
-  return <View />;
+  useEffect(() => {
+    if (user) {
+      getUserPosts(user.id);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerTitle: user?.nickName ?? '',
+      header: props => <Header {...props} rightIcon="logout" onPressRightIcon={handleLogout} />,
+    });
+  }, [user]);
+
+  return (
+    <AppLayout isScroll disableBottomInsets>
+      {user ? (
+        <View style={styles.container}>
+          <ProfileInfo user={user} />
+          <Button value="Edit Profile" icon={{ name: 'pencil', size: 16 }} />
+
+          {posts.length && postsMeta ? (
+            <ProfilePostGrid posts={posts} postsMeta={postsMeta} />
+          ) : (
+            <ProfilePostGridPlaceholder />
+          )}
+        </View>
+      ) : (
+        <View style={styles.placeholderContainer}>
+          <ProfileInfoPlaceholder />
+        </View>
+      )}
+    </AppLayout>
+  );
 };
 
 export default observer(ProfileDetailsView);
