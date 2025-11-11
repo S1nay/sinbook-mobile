56b164409d59700e2309d93aed0ffc46079b7080
Sharkov Nikita <bigbasspoll@gmail.com>
2025-11-11 20:08:52 +0300

    feat: created file api

---
+++ src/data/api/file/index.ts
@@ -0,0 +1,2 @@
+export { default } from './FileApi';
+export { IFileApi } from './IFileApi';
--- src/domain/models/index.ts
+++ src/domain/models/index.ts
@@ -2,3 +2,4 @@ export { IUser } from './IUser';
 export { IPost } from './IPost';
 export { IPagination } from './IPagination';
 export { IMeta } from './IMeta';
+export { IFile } from './IFile';
