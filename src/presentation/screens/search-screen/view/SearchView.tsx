05cc0b642ba1015b7fd0dd2914b25ef7160581ad
Sharkov Nikita <bigbasspoll@gmail.com>
2026-05-14 19:14:49 +0300

    feat: added search input and searching logic

--- src/presentation/screens/search-screen/view/SearchView.tsx
+++ src/presentation/screens/search-screen/view/SearchView.tsx
@@ -1,6 +1,6 @@
 import { useNavigation } from '@react-navigation/native';
 import { observer } from 'mobx-react-lite';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { ListRenderItemInfo } from 'react-native';
 
 import Placeholders from '@components/placeholders';
@@ -11,6 +11,7 @@ import { IPost, IUser } from '@domain/models';
 import AppLayout from '@layouts/_app';
 import { MaintenanceRouteNames, MaintenanceScreenProps } from '@navigation/configuration';
 import List from '@ui/list';
+import SearchInput from '@ui/search-input';
 import TopTabs from '@ui/tabs';
 
 import { ISearchViewModel } from '../view-model';
@@ -46,27 +47,44 @@ const SearchView = () => {
     useNavigation<MaintenanceScreenProps<MaintenanceRouteNames.Tab>['navigation']>();
 
   const usersLoaded = useRef(false);
+  const [activeTabIndex, setActiveTabIndex] = useState(0);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     loadPosts();
   }, []);
 
   const onTabIndexChange = (index: number) => {
+    setActiveTabIndex(index);
     if (index === USERS_TAB_INDEX && !usersLoaded.current) {
       usersLoaded.current = true;
       loadUsers();
     }
   };
 
-  const onPaginatePosts = async (page: number) => loadMorePosts(page);
-  const onPaginateUsers = async (page: number) => loadMoreUsers(page);
+  const handleSearch = (query: string) => {
+    setSearch(query);
+
+    if (activeTabIndex === USERS_TAB_INDEX) {
+      if (usersLoaded.current) loadUsers(query);
+    } else {
+      loadPosts(query);
+    }
+  };
+
+  const onPaginatePosts = async (page: number) => loadMorePosts(page, search);
+  const onPaginateUsers = async (page: number) => loadMoreUsers(page, search);
 
   const navigateToUserDetails = (userId: number, nickName: string) =>
     navigation.navigate(MaintenanceRouteNames.UserDetails, { userId, nickName });
 
   const renderPost = ({ item: post }: ListRenderItemInfo<IPost>) => <Post post={post} />;
   const renderUser = ({ item: user }: ListRenderItemInfo<IUser>) => (
-    <UserCard {...user} onPressUserName={() => navigateToUserDetails(user.id, user.nickName)} />
+    <UserCard
+      {...user}
+      onPressUserName={() => navigateToUserDetails(user.id, user.nickName)}
+      style={styles.userCard}
+    />
   );
 
   return (
@@ -80,6 +98,8 @@ const SearchView = () => {
             onPaginate={onPaginatePosts}
             onRefresh={refreshPosts}
             isLoading={isPostsLoading}
+            ListHeaderComponent={<SearchInput onDebouncedChange={handleSearch} />}
+            contentContainerStyle={styles.postListContainer}
             isRefreshing={isPostsRefreshing}
             placeholder={<Placeholders.PostListPlaceholder />}
           />
@@ -91,8 +111,9 @@ const SearchView = () => {
             pagination={usersMeta}
             onPaginate={onPaginateUsers}
             onRefresh={refreshUsers}
-            contentContainerStyle={styles.userListContainer}
             isLoading={isUsersLoading}
+            ListHeaderComponent={<SearchInput onDebouncedChange={handleSearch} />}
+            contentContainerStyle={styles.userListContainer}
             isRefreshing={isUsersRefreshing}
             placeholder={<Placeholders.UserCardListPlaceholder />}
           />
--- src/presentation/screens/search-screen/view/styles.ts
+++ src/presentation/screens/search-screen/view/styles.ts
@@ -1,7 +1,15 @@
 import { StyleSheet } from 'react-native-unistyles';
 
 export default StyleSheet.create({
-  userListContainer: {
+  postListContainer: {
+    paddingBottom: 24,
+    paddingTop: 0,
+  },
+  userCard: {
     paddingHorizontal: 16,
+  },
+  userListContainer: {
+    paddingBottom: 24,
+    paddingTop: 0,
   },
 });
