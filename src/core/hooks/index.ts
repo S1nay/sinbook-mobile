06665bb48d4e199ae058682b909cbd3cdb8b266c
Sharkov Nikita <bigbasspoll@gmail.com>
2026-05-14 19:09:55 +0300

    feat: added useUseCase hook

--- src/core/hooks/index.ts
+++ src/core/hooks/index.ts
@@ -1,6 +1,7 @@
 import useAuth from './use-auth';
 import useDIContainer from './use-DI-container';
 import usePagination from './use-pagination';
+import useUseCase from './use-use-case';
 import useTheme from './useTheme';
 
-export { useDIContainer, useAuth, usePagination, useTheme };
+export { useDIContainer, useAuth, usePagination, useTheme, useUseCase };
