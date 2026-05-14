fde90d61f302487367027725e4b6d547ffdec54e
Sharkov Nikita <bigbasspoll@gmail.com>
2026-05-14 19:16:47 +0300

    feat: hide three dots in post when is not auth user post

--- src/presentation/components/post/Post.tsx
+++ src/presentation/components/post/Post.tsx
@@ -1,7 +1,12 @@
 import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
 import { Dimensions, Pressable, Text, View } from 'react-native';
 import { useUnistyles } from 'react-native-unistyles';
 
+import { useUseCase } from '@core/hooks';
+import { IUser } from '@domain/models';
+import { UserUseCases } from '@domain/use-cases';
+import { GetCurrentUserUseCase } from '@domain/use-cases/user';
 import Avatar from '@ui/avatar';
 import Icon from '@ui/icon';
 import ImageCarousel from '@ui/image-carousel';
@@ -14,6 +19,12 @@ const POST_IMAGE_SIZE = Dimensions.get('screen').width;
 const Post = (props: PostProps) => {
   const { post } = props;
   const { theme } = useUnistyles();
+  const [currentUser, setCurrentUser] = useState<IUser | null>(null);
+  const getUserUseCase = useUseCase<GetCurrentUserUseCase>(UserUseCases.$GetCurrentUser);
+
+  useEffect(() => {
+    getUserUseCase.execute(true).then(setCurrentUser);
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -29,14 +40,16 @@ const Post = (props: PostProps) => {
           </View>
         </View>
 
-        <Pressable hitSlop={8}>
-          <Icon
-            size={24}
-            name="threeDots"
-            stroke={theme.colors.foreground.primary}
-            fill={theme.colors.foreground.primary}
-          />
-        </Pressable>
+        {currentUser?.id === post.user.id && (
+          <Pressable hitSlop={8}>
+            <Icon
+              size={24}
+              name="threeDots"
+              stroke={theme.colors.foreground.primary}
+              fill={theme.colors.foreground.primary}
+            />
+          </Pressable>
+        )}
       </View>
 
       <Text style={styles.postContent}>{post.content}</Text>
